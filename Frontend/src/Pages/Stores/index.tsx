20770573b4581d4fafc14aa97ba69b4be544d68b
aily26092001 <92494764+aily26092001@users.noreply.github.com>
2022-09-17 17:16:18 +0700

    CRUDStore Success

--- Frontend/src/Pages/Stores/index.tsx
+++ Frontend/src/Pages/Stores/index.tsx
@@ -1,103 +1,168 @@
 
-import React,{useState,useEffect} from 'react';
-import {  GetAllStore,DeteleStore } from '../../Services/Stores.Services';
+import React, { useState, useEffect } from 'react';
+import { GetAllStore, DeteleStore, CreateStore, UpdateStore } from '../../Services/Stores.Services';
 import { IStoreResult } from './../../Interfaces/IStoreServices';
-import {EditOutlined,DeleteOutlined} from '@ant-design/icons';
-import { Modal, Table } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Modal,Button, Table } from 'antd';
+import ModalStore from './CUStores';
+import { PlusOutlined  } from "@ant-design/icons";
 
 
 
 
 const Store = () => {
     const [stores, setStores] = useState<IStoreResult[]>([]);
-
+    const [storesEdit, setstoresEdit] = useState({});
+    const [isOpenModal, setisOpenModal] = useState(false);
     useEffect(() => {
         const fetchData = async () => {
             const results = await GetAllStore();
             setStores(results);
-            
-          };
-          fetchData();
+
+        };
+        fetchData();
     }, [stores])
-    
-const columns=[
-    {
-        key: 'storeId',
-        title : 'Id',
-        dataIndex :'storeId'
-    },
-    {
-        
-        key: 'storeName',
-        title : 'store name',
-        dataIndex :'storeName'
-    },
-    {
-        
-        key: 'phone',
-        title : 'phone',
-        dataIndex :'phone'
-    },
-    {
-        
-        key: 'email',
-        title : 'email',
-        dataIndex :'email'
-    },
-    {
-        
-        key: 'street',
-        title : 'street',
-        dataIndex :'street'
-    },
-    {
-        
-        key: 'city',
-        title : 'city',
-        dataIndex :'city'
-    },
-    {
-        
-        key: 'zipCode',
-        title : 'zip Code',
-        dataIndex :'zipCode'
-    },
-    {
-        key :'action',
-        title : 'Action',
-        render : (record : any)=>
+
+    const columns = [
         {
-            return<>
-            <EditOutlined  />
-            <DeleteOutlined  onClick={()=>handleDeleteCategory(record.storeId)} style={{color : "orange",marginLeft : 12}}/>
-            </>
+
+            key: 'storeName',
+            title: 'store name',
+            dataIndex: 'storeName'
+        },
+        {
+
+            key: 'phone',
+            title: 'phone',
+            dataIndex: 'phone'
+        },
+        {
+
+            key: 'email',
+            title: 'email',
+            dataIndex: 'email'
+        },
+        {
+
+            key: 'street',
+            title: 'street',
+            dataIndex: 'street'
+        },
+        {
+
+            key: 'city',
+            title: 'city',
+            dataIndex: 'city'
+        },
+        {
+
+            key: 'state',
+            title: 'state',
+            dataIndex: 'state'
+        },
+        {
+
+            key: 'zipCode',
+            title: 'zip Code',
+            dataIndex: 'zipCode'
+        },
+        {
+            key: 'action',
+            title: 'Action',
+            render: (record: any) => {
+                return <>
+                    <EditOutlined  onClick={()=>{showModal(record)}}/>
+                    <DeleteOutlined onClick={() => handleDeleteStore(record)} style={{ color: "orange", marginLeft: 12 }} />
+                </>
+            }
+
         }
-      
-    }
-]
-    const handleDeleteCategory=(id : number)=>
+    ]
+    const showModal =(record : any) =>
     {
+         console.log("idStore: "+record.storeId);
+        // console.log("ListStore: "+JSON.stringify(record));
+        
+        setisOpenModal(true);
+        setstoresEdit(record);
+    }
+    const hideModal = ()=>
+    {
+        setisOpenModal(false);
+    }
+    const handleDeleteStore = (record: any) => {
         Modal.confirm(
             {
-                title :"Are u sure?",
-                okText :'Sure',
-                okType : 'danger',
-                onOk : ()=>
-                {
-                    // gọi api delete rồi fetch data lại nè
-                     DeteleStore(id);
-                     //console.log("id store:",id);
+                title: "Are u sure?",
+                okText: 'Sure',
+                okType: 'danger',
+                onOk: async () => {
+                    await console.log("idStore:", record.storeId);
+                    await DeteleStore(record);
+                    
                 }
+                
+
             })
-            
-            alert("Post deleted!");
+
+
     }
 
-  return (
-    <div>
-      <Table  columns={columns} dataSource={stores} />
-    </div>
-  );
+    const handleFinish = async(id:number,values : any) =>{
+            const isEdit = stores.findIndex((item)=> item.storeId === id)
+         if(isEdit >= 0)
+         {
+            await UpdateStore({storeId: id,...values})
+            .then((res) =>
+            {
+                const newlistCate = stores.map((item)=>
+                {
+                    if(item.storeId === id)
+                    {
+                       item.storeName=values.storeName;
+                       item.phone=values.phone;
+                       item.email=values.emails;
+                       item.city=values.city;
+                       item.street=values.street;
+                       item.state=values.state;
+                       item.zipCode=values.zipCode;
+                    }
+                    return item;
+                })
+                setStores(newlistCate);
+            }).catch((error)=>
+            {
+            }).finally(() => {
+                setisOpenModal(false);
+              });
+         }
+         else
+         {
+            
+        //await console.log("ListStoreCreate: "+JSON.stringify(values));
+        await CreateStore(values);
+
+        setisOpenModal(false);
+
+         }
+       
+     }
+
+    return (
+        <>
+            <Button onClick={showModal} type="primary" danger icon={<PlusOutlined />}>Create Store</Button>
+            {
+                isOpenModal &&
+                <ModalStore   isCreate={isOpenModal} item={storesEdit} title="Store" onCancel={hideModal} onFinish={handleFinish} >
+                </ModalStore>
+            }
+            <Table columns={columns} dataSource={stores} rowKey="storeId" />
+        </>
+
+
+
+
+    );
 };
 
 export default Store;

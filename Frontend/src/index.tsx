0a056e37056e61a744802286a97ee79b9aa26567
bin.pqh <binpro0203@gmail.com>
2022-09-19 10:04:48 +0700

    huhu lag quó

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-
 import "antd/dist/antd.min.css";
 import App from "./App";
-import store from "./Redux/store";
-
 import reportWebVitals from "./reportWebVitals";
-
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from './app/store';

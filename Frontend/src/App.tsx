06c260e80727fb0173ff4cc9317e009c108934ea
Tran Quang <tranquangbdhz@gmail.com>
2022-09-19 19:11:47 +0700

    fix Auth Route

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -22,25 +22,26 @@ function App() {
   return (
     <div className="App">
       <Routes>
-      <Route element={<PrivateRoute roles={["Admin"]} />}>
-        <Route path="/" element={<LayoutDefault content="Product" />}>
-          <Route index element={<Product />} />;
-        </Route>
-        <Route path="/category" element={<LayoutDefault content="Category" />}>
-          <Route index element={<Category />} />;
-        </Route>
-        <Route path="/store" element={<LayoutDefault content="Store" />}>
-          <Route index element={<Store />} />;
-        </Route>
-        <Route path="/brand" element={<LayoutDefault content="Brand" />}>
-          <Route index element={<Brand />} />;
+        <Route element={<PrivateRoute roles={["Admin"]} />}>
+          <Route path="/" element={<LayoutDefault content="Product" />}>
+            <Route index element={<Product />} />;
+          </Route>
+          <Route path="/category" element={<LayoutDefault content="Category" />}>
+            <Route index element={<Category />} />;
+          </Route>
+          <Route path="/store" element={<LayoutDefault content="Store" />}>
+            <Route index element={<Store />} />;
+          </Route>
+          <Route path="/brand" element={<LayoutDefault content="Brand" />}>
+            <Route index element={<Brand />} />;
+          </Route>
+
+          {/* khúc này thêm 1 page k có quyền khi mà chưa login
+         <Route path={'not-auth'} element={<NotAuth />} /> */}
         </Route>
 
         <Route path="/login" element={<LayoutDefault content="Login" />}>
-        <Route path="login" element={<Login/>} />
-        </Route>
-        {/* khúc này thêm 1 page k có quyền khi mà chưa login
-         <Route path={'not-auth'} element={<NotAuth />} /> */}
+          <Route index element={<Login />} />;
         </Route>
       </Routes>
     </div>
